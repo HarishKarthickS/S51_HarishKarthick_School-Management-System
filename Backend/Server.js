76933c9fc37f517dc07c43c35a00dbc677b07e63
HarishKarthickS <harishskarthick.s@gmail.com>
2024-04-13 14:06:07 +0530

    Connecting to mongoDB

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors');
-// const { startDatabase, isConnected } = require('./db/database');
+const { startDatabase, isConnected } = require('./db/database');
 app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json())
@@ -10,19 +10,19 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send({message:'Welcome to the amazing world of Learning'})
 })
-// app.get('/home', (req, res) => {
-//   res.json({
-//     message: isConnected() ? 'Database is connected' : 'Disconnected from database'
-//   })
-//   })
+app.get('/home', (req, res) => {
+  res.json({
+    message: isConnected() ? 'Database is connected' : 'Disconnected from database'
+  })
+  })
 app.get('/ping',(req,res)=>{
     res.send({message:'pong!, Welcome to the amazing world of Learning'})
 })
 
-// startDatabase()
-// .then(()=>{
+startDatabase()
+.then(()=>{
   app.listen(3000,async()=>{
     console.log('Starting Server ....🚀')
     console.log('Server started running on port 3000 🏃‍♂️')
 })
-// })
+})
